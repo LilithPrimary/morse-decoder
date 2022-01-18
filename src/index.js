0fb81e3de27400f35c48b70a5c479475fc4bb175
Denna <lilithprimary@gmail.com>
2022-01-18 14:18:30 +0300

    feat: it works

--- src/index.js
+++ src/index.js
@@ -38,8 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const preMorse = {
+        "11": "-",
+        "10": "."
+    }
+    const arr = [];
+    let exprCopy = expr;
+    for (let i = 0; i < expr.length; i += 10) {
+        arr.push(exprCopy.slice(0, 10));
+        exprCopy = exprCopy.slice(10);
+    }
+    arr.forEach ((el, i) => {
+        if (el !== "**********") {
+            el = (+el).toString();
+            let elCopy = el;
+            let res = "";
+            for (let i = 0; i < el.length; i += 2) {
+                res += preMorse[elCopy.slice(0, 2)];
+                elCopy = elCopy.slice(2);
+            }
+            arr[i] = res;
+        } else {
+            arr[i] = " ";
+        }
+})
+    return arr.map(el => el === " " ? " " : MORSE_TABLE[el]).join("");
 }
+console.log (decode("0000101110000011111100000010110000111010**********00000000110000111111**********00101111100000101110000011111100001111100000101110000000101100000011110000001111000000101000000011100000111110"))
 
 module.exports = {
     decode
